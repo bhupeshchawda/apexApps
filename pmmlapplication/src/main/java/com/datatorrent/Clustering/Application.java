149dd926f8b0ffa1ae33605b95ef6f75f8306e3b
dnbaheti11 <dnbaheti11@gmail.com>
2017-11-09 11:19:05 +0530

    changes in Clustering Application

--- pmmlapplication/src/main/java/com/datatorrent/Clustering/Application.java
+++ pmmlapplication/src/main/java/com/datatorrent/Clustering/Application.java
@@ -1,23 +1,23 @@
-//package com.datatorrent.Clustering;
-//
-//import org.apache.apex.malhar.lib.fs.GenericFileOutputOperator;
-//import org.apache.hadoop.conf.Configuration;
-//import com.datatorrent.api.DAG;
-//import com.datatorrent.api.StreamingApplication;
-//import com.datatorrent.api.annotation.ApplicationAnnotation;
-//import com.datatorrent.lib.io.fs.AbstractFileOutputOperator;
-//
-//@ApplicationAnnotation(name="PMML-Clustering-Scoring-App")
-//public class Application implements StreamingApplication
-//{
-//  @Override
-//  public void populateDAG(DAG dag, Configuration conf)
-//  {
-//    FileInputOp inputOp=dag.addOperator("inputOp",FileInputOp.class);
-//    ClusteringOperator scoringOperator=dag.addOperator("scoringOperator",ClusteringOperator.class);
-//    GenericFileOutputOperator.StringFileOutputOperator fileOutPutOp=dag.addOperator("fileOutPutOp",GenericFileOutputOperator.StringFileOutputOperator.class);
-//
-//    dag.addStream("scoringdata", inputOp.output, scoringOperator.input).setLocality(DAG.Locality.CONTAINER_LOCAL);
-//    dag.addStream("printoutput",scoringOperator.output,fileOutPutOp.input);
-//  }
-//}
+package com.datatorrent.Clustering;
+
+import org.apache.hadoop.conf.Configuration;
+import com.datatorrent.api.DAG;
+import com.datatorrent.api.StreamingApplication;
+import com.datatorrent.api.annotation.ApplicationAnnotation;
+import com.datatorrent.pmml.operator.ClusteringScoringOperator;
+
+@ApplicationAnnotation(name="PMML-Clustering-Scoring-App")
+public class Application implements StreamingApplication
+{
+  @Override
+  public void populateDAG(DAG dag, Configuration conf)
+  {
+    ClusteringInput inputOp = dag.addOperator("inputOp1", ClusteringInput.class);
+    inputOp.setEmitBatchSize(1);
+    ClusteringScoringOperator scoring = dag.addOperator("clusteringOperator", ClusteringScoringOperator.class);
+    ScoringOutputOperator logger = dag.addOperator("Logger1", ScoringOutputOperator.class);
+
+    dag.addStream("data to scoring", inputOp.scoringOut, scoring.input);
+    dag.addStream("scoring to output", scoring.output, logger.input);
+  }
+}
